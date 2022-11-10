de0eb147ef8bd35c49e900a89966737ce50be77d
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-10 15:25:08 +0500

    Added Categories component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return (
+    <main>
+      <div className="container"></div>
+    </main>
+  );
 }
 
 export default App;
