1ced82122d557f9d862d8301f8d57bad3fd8eb8a
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-11 14:30:41 +0500

    Using State to set Categories for buttons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategoris] = useState([]);
 
   return <main></main>;
 }
