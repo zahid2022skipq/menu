38a2280c102e08b28bd8861575042947f2b62f34
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-11 15:47:56 +0500

    Fileter all button and styles

--- src/Categories.js
+++ src/Categories.js
@@ -3,6 +3,9 @@ import React from "react";
 const Categories = ({ filterMenuItems }) => {
   return (
     <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterMenuItems("all")}>
+        All
+      </button>
       <button
         className="filter-btn"
         onClick={() => filterMenuItems("breakfast")}
