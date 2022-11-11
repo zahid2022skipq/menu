3c932283ecb5f346354e22aa6293bdf1e8f23339
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-11 14:55:04 +0500

    Added Component to render Images from data

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Menu = ({ items }) => {
-  return <div className="section-center">{}</div>;
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, category, price, img, desc } = menuItem;
+        return (
+          <article key={id}>
+            <img src={img} alt={title} />
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
